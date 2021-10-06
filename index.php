abd8288936275813bbb25cade1e637f423e13bbc
GabrielSchilliro <gabrielschilliro02@gmail.com>
2021-10-05 21:17:03 -0300

    Trabajo olimpiadas jurisdiccionales

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
-	<!-- Icono que aparece en la pestaña -->
-	<link rel="shortcut icon" href="../img/simbolo_medico.png" type="image/png">
+    <!-- Icono que aparece en la pestaña -->
+    <link rel="shortcut icon" href="img/simbolo_medico.png" type="image/png">
 	<!-- Fuentes -->
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<!-- Titulo -->
-	<title>Login</title>
 	<!-- Titulo -->
 	<title>Hospital Online</title>
 </head>
